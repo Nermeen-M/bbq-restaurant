0e57612d41827829e7c7545a810ada5fb1ea846f
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2023-03-03 22:41:40 +0100

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Texas fire</h1>
+      <Routes>
+        <Route path="/" element={<p>Home</p>} exact />
+        <Route path="/menu" element={<p>Menu</p>} />
+        <Route path="/menu/:categoryName" element={<p>Category</p>} />
+        <Route path="/menu/:categoryName/:productId" element={<p>Product</p>} />
+        <Route path="/contact" element={<p>Contact</p>} />
+      </Routes>
     </div>
   );
 }
