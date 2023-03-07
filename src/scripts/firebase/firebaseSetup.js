222847db37785e66e5e17a556084d041aa137c11
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2023-03-07 07:07:29 +0100

    Firebase setup

---
+++ src/scripts/firebase/firebaseSetup.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdwJHMDrQN19rIOIoV2Bkp0Ew6GZVRLpY",
+  authDomain: "fire-grill-bbq-restaurant.firebaseapp.com",
+  projectId: "fire-grill-bbq-restaurant",
+  storageBucket: "fire-grill-bbq-restaurant.appspot.com",
+  messagingSenderId: "100361334509",
+  appId: "1:100361334509:web:f003d6ff54fe664037a6e4",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const database = getFirestore(firebaseApp);
+export const cloudStorage = getStorage(firebaseApp);
